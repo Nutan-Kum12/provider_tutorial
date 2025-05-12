4872defc53fd5e6fc1e01c2076b47161ef90063d
Nutan-Kum12 <166277808+Nutan-Kum12@users.noreply.github.com>
2025-05-13 01:21:56 +0530

    Consumer use

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,30 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    print('build method called');
+    // this method is called when the widget is built
     return Scaffold(
       appBar:AppBar(
       title: Text('Home'),
     ),
     body: Center(
-      child: Text('${Provider.of<CounterProvider>(context,listen: true).getCount()}',style: TextStyle(
-        fontSize: 25,
-        color: Colors.red,
-      ),),
-    ),
+      // child: Text('${Provider.of<CounterProvider>(context,listen: true).getCount()}',
+      // this is the way to access the provider and when you use listen: true, it will rebuild the widget when the value changes
+      // it rebuild the whole UI of your homepage but you have to change only the text for it we have to use consumer
+      child: Consumer<CounterProvider>(
+        builder: (context, counterProvider, child) {
+          // in builder we passed three parameters context, counterProvider and child
+          // context is the build context of the widget
+          // counterProvider is the instance of the CounterProvider class
+          // child is the child widget that we can use to avoid rebuilding the widget
+          return Text('${counterProvider.getCount()}',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.red,
+          ),);
+        },
+      ),
+     ),
     floatingActionButton: FloatingActionButton(
       onPressed: ()
       {
